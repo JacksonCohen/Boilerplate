a3c4c12ca6ee09bf8d6ee697df8c82e6889d2117
Jackson <jacksonrcohen@gmail.com>
2019-04-26 19:32:34 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 7000;
 
 app.use(express.static(path.join(__dirname, './client/dist')))
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up and running on port ${port}!`);
 });
