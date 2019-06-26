a2854c566293ae12cbe6704176c598db75558c38
Michael Roeslein <michaeljroeslein@gmail.com>
2019-06-26 17:44:35 -0400

    Updated Boilerplate

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
 const port = 7000;
 
-app.use(express.static(path.join(__dirname, './client/dist')))
-
+app.use(express.static("client/dist"));
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}!`);
